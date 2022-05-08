7f203cfff8bb8040bf67cf9c88d5f707ea247072
Riccardo De Silva <riccardo.desilva@gmail.com>
2022-05-08 10:26:02 +0200

    Aggiunta configurazione runtime tramite json, caricate immagini originali indovina chi, accorgimenti grafici

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import './index.css';
 function GameBoard(props) {
   return (
     <div className="gameboard">
-      {props.characters}
+      {props.characters.map((character, index)=>{
+        return(
+            <div key={index} className="gameboard__item">{character}</div>
+        );
+      })}
     </div>
   );
 }
@@ -18,76 +22,155 @@ class CharacterCard extends React.Component {
         
         return (
             <div className={classList} onClick={this.props.onClick}>
-              <img className="character-card__img" alt={this.props.name} />
+              <img className="character-card__img" alt={this.props.name} src={this.props.image}/>
               <p className="character-card__name">{this.props.name}</p>
             </div>
         );
     }
 }
 
-class Game extends React.Component {
+function Modal(props) {
     
+    const classList = props.show ? "modal show" : "modal";
+    
+    return(
+        <div className={classList}>
+            <div className="modal__container">
+                <div className="modal__header">
+                    <h3>{props.title}</h3>
+                    <button className="btn btn-close" onClick={props.handleClose}>X</button>
+                </div>
+                <div className="modal__body">
+                    {props.children}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+class Game extends React.Component {
     
     constructor(props) {
         super(props);
         this.state = {
-            characters: this.getDefaultCharacters(24),
-            yourCharacter: this.getRandomCharacterKey(24)
+            initializing: true,
+            showModal: false,
+            companyName: "",
+            companyLogoUrl: "",
+            charactersData: null,
+            characters: null,
+            randomCharacterData: null
         };
         this.handleCharacterClick = this.handleCharacterClick.bind(this);
         this.restartGame = this.restartGame.bind(this);
-        this.getDefaultCharacters = this.getDefaultCharacters.bind(this);
-        this.getRandomCharacterKey = this.getRandomCharacterKey.bind(this);
-    }                                             
+        this.getCharactersFromData = this.getCharactersFromData.bind(this);
+        this.getRandomCharacterData = this.getRandomCharacterData.bind(this);
+        this.initialize = this.initialize.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+        
+        this.initialize();
+    }
+    
+    initialize() {
+        fetch('data/settings.json').then((res)=>res.json()).then(data => {
+            
+            const companyName = data.company.name;
+            const companyLogoUrl = data.company.logo;
+            const charactersData = data.characters.sort();
+            const characters = this.getCharactersFromData(charactersData);
+            const randomCharacterData = this.getRandomCharacterData(charactersData);
+            
+            this.setState({
+                initializing: false,
+                companyName: companyName,
+                companyLogoUrl: companyLogoUrl,
+                charactersData: charactersData,
+                characters: characters,
+                randomCharacterData: randomCharacterData
+            })
+        });
+    }
+    
+    closeModal() {
+        this.setState({showModal: false});
+    }
           
-    handleCharacterClick(characterKey) {
+    handleCharacterClick(characterName) {
         this.setState({characters: this.state.characters.map(character => {
-            if (character.props.name == characterKey && character.props.closed === false) {
+            if (character.props.name === characterName && character.props.closed === false) {
                 return (
-                    <CharacterCard name={characterKey} key={characterKey} closed={true} onClick={()=>this.handleCharacterClick(characterKey)}/>
+                    <CharacterCard name={characterName} closed={true} image={character.props.image} onClick={()=>this.handleCharacterClick(characterName)}/>
                 );
             }
             
             return character;
         })});
+        
     }
     
-    getRandomCharacterKey(num) {
-        return Math.floor(Math.random()*num);
+    getRandomCharacterData(charactersData) {
+        const randomIndex = Math.floor(Math.random()*charactersData.length); 
+        return charactersData[randomIndex];
     }
         
-    getDefaultCharacters(num) {
-        return(
-            [...Array(num).keys()].map(key => (
-                <CharacterCard name={key} key={key} closed={false} onClick={()=>this.handleCharacterClick(key)}/>
-            ))
-        );
+    getCharactersFromData(data) {
+        
+        const isUnique = (arr) => {
+            const tmpArr = [];
+            for(var obj in arr) {
+                if(tmpArr.indexOf(arr[obj].name) < 0){ 
+                    tmpArr.push(arr[obj].name);
+                } else {
+                    return false; // Duplicate value for property1 found
+                }
+            }
+            return true;
+        };
+        
+        const isValidCharactersNumber = (number) => number % 4 === 0;
+        
+        if (isValidCharactersNumber(data.length) && isUnique(data)) {
+            return data.map((character, index) => {
+                return(
+                    <CharacterCard name={character.name} image={character.image} closed={false} onClick={()=>this.handleCharacterClick(character.name)}/>
+                );
+            });
+        } else {
+            throw new Error("Errore nel file di configurazione");
+        }
     }
         
     restartGame() {
+        
         this.setState({
-            characters: this.getDefaultCharacters(24),
-            yourCharacter: this.getRandomCharacterKey(24)
+            characters: this.getCharactersFromData(this.state.charactersData),
+            randomCharacterData: this.getRandomCharacterData(this.state.charactersData)
         });
+        
     }
                                                           
   render() {
-    return (
-        <>
-        <header className="header">
-            <img className="header__logo" alt="" src="logo.svg" />
-        </header>
-        <main className="main">
-            <div className="maincontent">
-                <GameBoard characters={this.state.characters}/>
-            </div>
-            <div className="sidebar">
-                <CharacterCard name={this.state.yourCharacter}/>
-                <button className="btn btn-reset" onClick={this.restartGame}>Reset</button>
-            </div>
-        </main>
-        </>
-    );
+      if (this.state.initializing) {
+          return(<><h1>Loading...</h1></>);
+      } else {
+          return (
+            <>
+            <header className="header">
+                <img className="header__logo" alt={this.state.companyName} src={this.state.companyLogoUrl} />
+            </header>
+            <main className="main">
+                <div className="maincontent">
+                    <GameBoard characters={this.state.characters}/>
+                </div>
+                <div className="sidebar">
+                    <CharacterCard name={this.state.randomCharacterData.name} image={this.state.randomCharacterData.image}/>
+                    <button className="btn btn-reset" onClick={this.restartGame}>Reset</button>
+                </div>
+            </main>
+            <Modal show={this.state.showModal} handleClose={()=>this.closeModal()} />
+            </>
+        );
+      }
   }
 }
 
